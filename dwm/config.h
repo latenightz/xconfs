4f17e8803c350b26d97d1fa4ce9700da637a412b
latenightz <catchyname12345@gmail.com>
2021-06-28 10:05:17 -0400

    Changed MODKEY to Windows Key + enabled bottom bar

--- dwm/config.h
+++ dwm/config.h
@@ -4,14 +4,14 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#2f0606";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#d46e08";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#06a829";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -38,13 +38,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "TILED",      tile },    /* first entry is default */
+	{ "FLOAT",      NULL },    /* no layout function means floating behavior */
+	{ "MONOC",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask // windows key
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
--- slstatus/config.h
+++ slstatus/config.h
@@ -65,8 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
-	{ battery_perc, " [BAT %s%] ", "BAT0" },
+	{ datetime, "%s ",           "%F %T" },
 	{ cpu_perc, "[CPU %s%] ", NULL },
 	{ ram_perc, "[RAM %s%] ", NULL },
 };
