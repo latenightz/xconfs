781e00345dce35584547c13139ee08e3ffbedfee
latenightz <justaidenlol@gmail.com>
2021-06-28 14:05:33 -0400

    fixed the tree

--- dwm/config.h
+++ dwm/config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+static const char *tags[] = { "web", "dev", "git", "4", "5", "6", "7", "8", "9", "10" };
 
 static const Rule rules[] = {
 	/* xprop(1):
