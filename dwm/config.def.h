2becb46e926c10f22f65d83994aaa73730c03fbd
latenightz <justaidenlol@gmail.com>
2021-06-28 15:48:52 -0400

    Sloppy, but now there's some icons! Term+Firefox, requires Fontawesome.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -13,11 +13,6 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
