65180089899e6d2450a6858214d626e63103e8ee
latenightz <catchyname12345@gmail.com>
2021-07-09 16:12:24 -0400

    More fun bindings! Note: changed some bindings, like with NERDTree. It's CTRL+M.

--- dwm/config.h
+++ dwm/config.h
@@ -7,13 +7,14 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14", "fontawesome:size=14" };
-static const char dmenufont[]       = "monospace:size=18";
+static const char *fonts[]          = { "iosevka:size=14", "fontawesome:size=14" };
+static const char dmenufont[]       = "freemono:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#2f0606";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#06a829";
+static const char def_terminal[]    = "alacritty"; // Default terminal defined here!
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -32,7 +33,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "Alacritty", NULL,      NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
@@ -61,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { def_terminal, NULL };
 static const char *webbrowser[] = { "firefox", NULL };
 static const char *volup[] = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL  };
 static const char *voldown[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL  };
--- vimrc
+++ vimrc
@@ -1,19 +1,48 @@
-"" plugins
+""""""" plugins
 call plug#begin()
-  Plug 'preservim/nerdtree'
-  Plug 'ervandew/supertab'
+  Plug 'ffanzhang/vim-airline-stopwatch' " stopwatch no-one uses
+  Plug 'ervandew/supertab' " autocomplete
+  Plug 'vim-airline/vim-airline' " statusbar
+  Plug 'preservim/nerdtree' " file manager in VIM!
 call plug#end()
 
-"" misc, mainly 'sets'
-syntax on
+"""""" misc, mainly 'sets'
+syntax on " syntax highlighting on/off
 colorscheme gruvbox
-set backspace=indent,eol,start
-set bg=dark
+set backspace=indent,eol,start " fixed the backspace glitch
+set bg=dark " Remember to always set this
 set cursorline
-set number relativenumber
-set wildmenu
+set number relativenumber " the cool number lines
+set wildmenu " for easier vim stuff
 
-"" bindings
+"""""" bindingss
+"
+"Disabled for now, I don't really use this often.
+"map <F5> :call airline#extensions#stopwatch#run()<CR>
+"map <F6> :call airline#extensions#stopwatch#split()<CR>
+"map <F7> :call airline#extensions#stopwatch#stop()<CR>
+"map <F8> :call airline#extensions#stopwatch#reset()<CR>
+"map <F9> :call airline#extensions#stopwatch#summary()<CR> 
+
+" splits
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+
+" tabs
+nnoremap <C-I> :tabp<CR>
+nnoremap <C-O> :tabn<CR>
+
+" file I/O/Q
+nnoremap <C-W> :w<CR>
+nnoremap <C-Q> :q<CR>
+nnoremap <C-F> /
+
+" v/h/splits
+nnoremap <C-P> :vsplit<CR>
+
+" movelines & nerdtree (general <Leader> stuff)
 nnoremap <F4> :m .-2<CR>
 nnoremap <F3> :m .+1<CR>
-nnoremap <Leader>nt :NERDTree<CR>
+nnoremap <C-M> :NERDTree<CR>
